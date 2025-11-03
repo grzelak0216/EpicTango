f61b2f5b90209d9d9c1b5362a35bda1fb1a70ede
grzelak0216 <grzelak0216@gmail.com>
2025-11-03 18:48:16 +0100

    make fix

--- Skrypty/install_epics_rpi4.sh
+++ Skrypty/install_epics_rpi4.sh
@@ -6,10 +6,10 @@ sudo apt update
 sudo apt install -y \
     git build-essential libreadline-dev libncurses-dev perl libexpat1-dev \
     libtool automake pkg-config libssl-dev flex bison \
-    python3-dev python3-numpy python3-pip ca-certificates wget curl
+    python3-dev python3-numpy python3-pip python3-venv \
+    ca-certificates wget curl
 
-echo "==== Instalacja pyepics i caproto ===="
-sudo apt install -y python3-venv
+echo "==== Tworzenie środowiska Pythona ===="
 python3 -m venv ~/epics-venv
 source ~/epics-venv/bin/activate
 pip install pyepics caproto
@@ -20,7 +20,10 @@ mkdir -p "$EPICS_ROOT"/modules
 cd "$EPICS_ROOT"
 
 echo "==== Pobieranie i budowanie EPICS Base ===="
-git clone --branch R7.0.8 https://github.com/epics-base/epics-base.git
+if [ ! -d epics-base ]; then
+    git clone --branch R7.0.8 https://github.com/epics-base/epics-base.git
+fi
+
 cd epics-base
 EPICS_HOST_ARCH=$(./startup/EpicsHostArch)
 make -j$(nproc)
@@ -34,27 +37,45 @@ export EPICS_BASE=\$EPICS_ROOT/epics-base
 export EPICS_MODULES=\$EPICS_ROOT/modules
 export PATH=\$EPICS_BASE/bin/$EPICS_HOST_ARCH:\$PATH
 export LD_LIBRARY_PATH=\$EPICS_BASE/lib/$EPICS_HOST_ARCH:\$LD_LIBRARY_PATH
+source ~/epics-venv/bin/activate
 EOF
 source ~/.bashrc
+
+# funkcja do poprawiania RELEASE
+fix_release() {
+    local dir="$1/configure/RELEASE"
+    if [ -f "$dir" ]; then
+        sed -i "s|^EPICS_BASE *=.*|EPICS_BASE = $EPICS_ROOT/epics-base|" "$dir"
+    fi
+}
 
 echo "==== Pobieranie i budowanie modułów: asyn, seq, stream ===="
 cd "$EPICS_ROOT/modules"
 
 # asyn
-git clone https://github.com/epics-modules/asyn.git
+if [ ! -d asyn ]; then
+    git clone https://github.com/epics-modules/asyn.git
+fi
+fix_release asyn
 cd asyn
 make -j$(nproc)
 
 # seq
 cd "$EPICS_ROOT/modules"
-git clone https://github.com/epics-modules/sequencer.git
+if [ ! -d sequencer ]; then
+    git clone https://github.com/epics-modules/sequencer.git
+fi
+fix_release sequencer
 cd sequencer
 make -j$(nproc)
 
 # stream
 cd "$EPICS_ROOT/modules"
-git clone https://github.com/paulscherrerinstitute/StreamDevice.git stream
+if [ ! -d stream ]; then
+    git clone https://github.com/paulscherrerinstitute/StreamDevice.git stream
+fi
+fix_release stream
 cd stream
 make -j$(nproc)
 
-echo "==== Instalacja zakończona pomyślnie (Raspberry Pi) ===="
+echo "==== Instalacja zakończona pomyślnie (Raspberry Pi / Ubuntu) ===="
