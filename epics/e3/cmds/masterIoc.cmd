d6634576422b9f8be4de39adcf06d222abeca2df
grzelak0216 <grzelak0216@gmail.com>
2026-06-03 22:15:00 +0200

    e3 record fix

--- epics/e3/cmds/masterIoc.cmd
+++ epics/e3/cmds/masterIoc.cmd
@@ -1,39 +1,33 @@
-
-
 cd "${TOP}"
 
 ## Konfig PVA
-epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.30.100")
+epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.20.101 192.168.30.100")
 epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST", "NO")
 epicsEnvSet("EPICS_PVA_BROADCAST_PORT", "5076")
 epicsEnvSet("EPICS_PVA_SERVER_PORT", "5075")
 
-## Konfig IOC_v1 (local)
-epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.20.101 192.168.30.100")
+## Konfig CA (local)
+epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.30.100")
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
 epicsEnvSet("EPICS_CA_SERVER_PORT", "5064")
 epicsEnvSet("EPICS_CA_REPEATER_PORT", "5065")
 
-## PVA server start
+## PVA server start - should be after environment setup
 pvaSrvStart
 
 ## Load record instances
 dbLoadRecords "db/router.db" 
 dbLoadRecords "db/versionIOC.db", "user=glak"
 
-## Konfiguracja channel access client dla komunikacji z IOC_v2
-dbLoadRecords "db/remoteConnections.db", "REMOTE_IP=192.168.30.121"
-
-## Definicje PV dla komunikacji PVA
-dbLoadRecords "db/pvaBridge.db", "IOC_V2=192.168.30.121"
+## Fixed: Correct IP addresses and record names
+dbLoadRecords "db/remoteConnections.db", "REMOTE_IP=192.168.20.101"
+dbLoadRecords "db/pvaBridge.db", "REMOTE_IOC_IP=192.168.20.101"
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-## Po inicjalizacji - ustawienia dodatkowe
-# Utworzenie połączeń PVA z IOC_v2
+## Fixed: Use correct PV names based on your database
 pvaLinkConnect("IOC_V2:version")
 pvaLinkConnect("IOC_V2:status")
 
-
-dbpf "CONNECTION_STATUS", "Connected to 192.168.30.121"
+dbpf "CONNECTION_STATUS", "Connected to 192.168.20.101"
--- epics/e3/cmds/routerIoc.cmd
+++ epics/e3/cmds/routerIoc.cmd
@@ -1,20 +1,17 @@
-#!../../bin/linux-x86_64/connectorIOC
-< envPaths
-
 cd "${TOP}"
 
-## Register all support components
-dbLoadDatabase "dbd/connectorIOC.dbd"
-connectorIOC_registerRecordDeviceDriver pdbbase
-
-## Konfiguracja PVA dla komunikacji z IOC_v2 (192.168.30.121)
-epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.30.121")
+## Fixed: Added local IP to PVA address list
+epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.20.101 
+                                    192.168.30.100 
+                                    192.168.10.100 
+                                    192.168.10.101 
+                                    192.168.10.102")
 epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST", "NO")
 epicsEnvSet("EPICS_PVA_BROADCAST_PORT", "5076")
 epicsEnvSet("EPICS_PVA_SERVER_PORT", "5075")
 
-## Konfiguracja dla IOC_v1 (lokalny adres)
-epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.10.105 192.168.30.121")
+## Konfig CA
+epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.20.101")
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
 epicsEnvSet("EPICS_CA_SERVER_PORT", "5064")
 epicsEnvSet("EPICS_CA_REPEATER_PORT", "5065")
@@ -26,19 +23,14 @@ pvaSrvStart
 dbLoadRecords "db/router.db" 
 dbLoadRecords "db/versionIOC.db", "user=glak"
 
-## Konfiguracja channel access client dla komunikacji z IOC_v2
-dbLoadRecords "db/remoteConnections.db", "REMOTE_IP=192.168.30.121"
-
-## Definicje PV dla komunikacji PVA
-dbLoadRecords "db/pvaBridge.db", "IOC_V2=192.168.30.121"
+## Fixed: Correct IP address (was 192.168.30.121, should be 192.168.30.100)
+dbLoadRecords "db/remoteConnections.db", "REMOTE_IP=192.168.30.100"
+dbLoadRecords "db/pvaBridge.db", "REMOTE_IOC_IP=192.168.30.100"
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-## Po inicjalizacji - ustawienia dodatkowe
-# Utworzenie połączeń PVA z IOC_v2
 pvaLinkConnect("IOC_V2:version")
 pvaLinkConnect("IOC_V2:status")
 
-## Monitorowanie połączeń
-dbpf "CONNECTION_STATUS", "Connected to 192.168.30.121"
+dbpf "CONNECTION_STATUS", "Connected to 192.168.30.100"
