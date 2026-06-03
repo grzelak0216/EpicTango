95427adf4e3b7b0428138043cb485185e890f364
grzelak0216 <grzelak0216@gmail.com>
2026-06-03 22:48:44 +0200

    Db record fix

--- epics/e3/cmds/masterIoc.cmd
+++ epics/e3/cmds/masterIoc.cmd
@@ -1,18 +1,21 @@
-## Konfig PVA
-epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.20.101 192.168.30.100")
+## Ustawienia środowiskowe
 epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST", "NO")
+epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.20.101 192.168.30.100")
 epicsEnvSet("EPICS_PVA_BROADCAST_PORT", "5076")
 epicsEnvSet("EPICS_PVA_SERVER_PORT", "5075")
 
-## Konfig CA (local)
-epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.30.100")
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
-epicsEnvSet("EPICS_CA_SERVER_PORT", "5064")
-epicsEnvSet("EPICS_CA_REPEATER_PORT", "5065")
+epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.30.100")
 
-## Load record instances
-dbLoadRecords "../db/master.db"
+## Ustawienia projektu
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+epicsEnvSet("IOC_NAME", "MASTER")
+epicsEnvSet("REMOTE_IOC", "ROUTER")
+epicsEnvSet("REMOTE_IP", "192.168.20.101")
 
-iocInit
+## Ładowanie rekordów
+dbLoadRecords("$(TOP)/db/master.db", "IOC=$(IOC_NAME),REMOTE=$(REMOTE_IOC),REMOTE_IP=$(REMOTE_IP)")
 
-dbpf "CONNECTION_STATUS", "1"
+iocInit()
+
+dbpf "$(IOC):CONNECTION_STATUS", "1"
--- epics/e3/cmds/routerIoc.cmd
+++ epics/e3/cmds/routerIoc.cmd
@@ -1,18 +1,21 @@
-## Konfig PVA
-epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.20.101 192.168.30.100")
+## Ustawienia środowiskowe
 epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST", "NO")
+epicsEnvSet("EPICS_PVA_ADDR_LIST", "192.168.20.101 192.168.30.100")
 epicsEnvSet("EPICS_PVA_BROADCAST_PORT", "5076")
 epicsEnvSet("EPICS_PVA_SERVER_PORT", "5075")
 
-## Konfig CA
-epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.20.101")
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
-epicsEnvSet("EPICS_CA_SERVER_PORT", "5064")
-epicsEnvSet("EPICS_CA_REPEATER_PORT", "5065")
+epicsEnvSet("EPICS_CA_ADDR_LIST", "192.168.20.101")
 
-## Load record instances
-dbLoadRecords "../db/router.db"
+## Ustawienia projektu
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+epicsEnvSet("IOC_NAME", "ROUTER")
+epicsEnvSet("REMOTE_IOC", "MASTER")
+epicsEnvSet("REMOTE_IP", "192.168.30.100")
 
-iocInit
+## Ładowanie rekordów
+dbLoadRecords("$(TOP)/db/router.db", "IOC=$(IOC_NAME),REMOTE=$(REMOTE_IOC),REMOTE_IP=$(REMOTE_IP)")
 
-dbpf "CONNECTION_STATUS", "1"
+iocInit()
+
+dbpf "$(IOC):CONNECTION_STATUS", "1"
